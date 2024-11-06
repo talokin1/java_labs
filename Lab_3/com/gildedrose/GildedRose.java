e8dfc2048eed885f91f7de5f4c4d7585c7afc023
Mykola Bovan <Bovan494@gmail.com>
2024-11-06 16:17:59 +0200

    Added updateAgedBrie - method increases the quality by + 1 if it has not yet reached the maximum and aadditional quality increase occurs if sellIn is less than 0

--- Lab_3/com/gildedrose/GildedRose.java
+++ Lab_3/com/gildedrose/GildedRose.java
@@ -87,5 +87,21 @@ class GildedRose {
             item.quality -= 1;
         }
     }
+
+
+    private void updateAgedBrie(Item item) {
+        if (item.quality < 50) {
+            item.quality += 1;
+        }
+        if (item.sellIn < 0 && item.quality < 50) {
+            item.quality += 1;
+        }
+    }
+
+
+
 }
+
+
+
 
