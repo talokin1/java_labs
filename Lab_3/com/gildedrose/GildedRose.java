905bbaef4d42963c7fffa909e901265c276587c1
Mykola Bovan <Bovan494@gmail.com>
2024-11-07 13:38:33 +0200

    Added borderQuality - method sets the quality value between 0 and 50

--- Lab_3/com/gildedrose/GildedRose.java
+++ Lab_3/com/gildedrose/GildedRose.java
@@ -89,6 +89,16 @@ class GildedRose {
     }
 
 
+    private void borderQuality(Item item) {
+        if (item.quality > 50) {
+            item.quality = 50;
+        }
+        if (item.quality < 0) {
+            item.quality = 0;
+        }
+    }
+
+
 }
 
 
