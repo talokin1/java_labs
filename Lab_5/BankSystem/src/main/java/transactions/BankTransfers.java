dedbc7fd9731b3ae1417723336abb57b7b1e3081
Mykola Bovan <Bovan494@gmail.com>
2024-12-12 13:24:48 +0200

    Refactored BankTransfers

--- Lab_5/BankSystem/src/main/java/transactions/BankTransfers.java
+++ Lab_5/BankSystem/src/main/java/transactions/BankTransfers.java
@@ -1,4 +1,15 @@
 package transactions;
 
-public class BankTransfers {
+import payment.PaymentSystem;
+
+public class BankTransfers extends Transaction {
+
+    public BankTransfers(PaymentSystem paymentSystem) {
+        super(paymentSystem);
+    }
+
+    @Override
+    public String process(double amount) {
+        return paymentSystem.processPayment(amount) + "\nProcessing bank transfer...";
+    }
 }
