1caf61a4671ea681fa9f6a07885e053b2c58560f
Mykola Bovan <Bovan494@gmail.com>
2024-12-12 12:51:40 +0200

    Added test for AmazonPay

---
+++ Lab_5/BankSystem/src/test/java/payments/AmazonPayTest.java
@@ -0,0 +1,18 @@
+package payments;
+
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AmazonPayTest {
+
+    @Test
+    public void testAmazonPayProcessing() {
+        PaymentSystem paymentSystem = new AmazonPay();
+        String result = paymentSystem.processPayment(5000);
+
+        assertNotNull(result, "AmazonPay should return a result for payment processing");
+        assertTrue(result.contains("AmazonPay"), "Result should indicate that AmazonPay processed the payment");
+    }
+}
