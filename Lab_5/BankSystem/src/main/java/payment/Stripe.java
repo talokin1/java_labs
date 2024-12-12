28648675312e2b64b4afae80800a7cd2f4ac9625
Mykola Bovan <Bovan494@gmail.com>
2024-12-12 13:09:03 +0200

    Refactored Stripe

--- Lab_5/BankSystem/src/main/java/payment/Stripe.java
+++ Lab_5/BankSystem/src/main/java/payment/Stripe.java
@@ -1,4 +1,9 @@
 package payment;
 
-public class Stripe {
+public class Stripe implements PaymentSystem {
+
+    @Override
+    public String processPayment(double amount) {
+        return "Stripe processed payment of $" + amount;
+    }
 }
