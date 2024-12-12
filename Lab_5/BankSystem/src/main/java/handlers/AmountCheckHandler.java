d4807542bfc9cb802e984ea5627157d56d5880c4
Mykola Bovan <Bovan494@gmail.com>
2024-12-12 13:59:46 +0200

    Refactored AmountCheckHandler

--- Lab_5/BankSystem/src/main/java/handlers/AmountCheckHandler.java
+++ Lab_5/BankSystem/src/main/java/handlers/AmountCheckHandler.java
@@ -1,4 +1,21 @@
 package handlers;
 
-public class AmountCheckHandler {
+
+import requests.TransactionRequest;
+
+public class AmountCheckHandler extends TransactionHandler {
+
+    Checks check = new Checks();
+
+    @Override
+    public boolean handleTransaction(TransactionRequest request) {
+        if (check.checkAmount(request)) {
+            System.out.println("Transaction exceeds the policy limit");
+            return false;
+        }
+
+        System.out.println("Amount successfully checked");
+        return next == null || next.handleTransaction(request);
+    }
+
 }
