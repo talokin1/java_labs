451e18ee52d885271b3b5c9c92839edd8b26f681
Mykola Bovan <Bovan494@gmail.com>
2024-12-12 13:56:51 +0200

    Refactored BalanceCheckHandler

--- Lab_5/BankSystem/src/main/java/handlers/BalanceCheckHandler.java
+++ Lab_5/BankSystem/src/main/java/handlers/BalanceCheckHandler.java
@@ -1,4 +1,21 @@
 package handlers;
 
-public class BalanceCheckHandler {
+import requests.TransactionRequest;
+
+public class BalanceCheckHandler extends TransactionHandler {
+
+    Checks check = new Checks();
+
+    @Override
+    public boolean handleTransaction(TransactionRequest request) {
+        if (check.checkBalance(request)) {
+            System.out.println("Insufficient funds on the balance sheet");
+            return false;
+        }
+
+        System.out.println("Balance successful checked");
+        return next == null || next.handleTransaction(request);
+
+    }
+
 }
