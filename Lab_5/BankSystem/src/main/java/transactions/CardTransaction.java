bdde4a910ba967151c62c387c82e5b0a6dc2bd13
Mykola Bovan <Bovan494@gmail.com>
2024-12-12 13:27:59 +0200

    Refactored CardTransaction

--- Lab_5/BankSystem/src/main/java/transactions/CardTransaction.java
+++ Lab_5/BankSystem/src/main/java/transactions/CardTransaction.java
@@ -1,4 +1,15 @@
 package transactions;
 
-public class CardTransaction {
+import payment.PaymentSystem;
+
+public class CardTransaction extends Transaction {
+
+    public CardTransaction(PaymentSystem paymentSystem) {
+        super(paymentSystem);
+    }
+
+    @Override
+    public String process(double amount) {
+        return paymentSystem.processPayment(amount) + "\nProcessing card payment...";
+    }
 }
