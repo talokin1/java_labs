66b8bd13bcc1b3394d7a56b6a85b9070e64240eb
Mykola Bovan <Bovan494@gmail.com>
2024-12-12 13:07:22 +0200

    Refactored PayPal

--- Lab_5/BankSystem/src/main/java/payment/PayPal.java
+++ Lab_5/BankSystem/src/main/java/payment/PayPal.java
@@ -1,4 +1,9 @@
 package payment;
 
-public class PayPal {
+public class PayPal implements PaymentSystem {
+
+    @Override
+    public String processPayment(double amount) {
+        return "PayPal processed payment of $" + amount;
+    }
 }
